b0dcc1b0d0f577d9e0e1d80fffc06932b70da287
Q-Xue <34317733+Q-Xue@users.noreply.github.com>
2022-05-09 21:54:35 +0800

    add some intro to stow

--- stow-script.zsh
+++ stow-script.zsh
@@ -1,3 +1,11 @@
+ref[1] https://www.youtube.com/watch?v=CFzEuBGPPPg&t=1183s
+
+# ------------------ stow basic flags-------------------#
+# stow -nvt
+# stow --adopted
+# stow -D
+
+
 # link git config files
 stow -vt ~ git
 # link zsh config files
