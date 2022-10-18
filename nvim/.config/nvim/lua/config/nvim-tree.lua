655c79c2c176af05960645a13f7270b06e33f28a
etg <eidano_x@qq.com>
2022-10-18 09:05:35 +0800

    trim the file

--- nvim/.config/nvim/lua/config/nvim-tree.lua
+++ nvim/.config/nvim/lua/config/nvim-tree.lua
@@ -1,73 +1,3 @@
--- -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
--- vim.g.nvim_tree_git_hl = 1
-
--- -- 0 by default, will enable folder and file icon highlight for opened files/directories.
--- vim.g.nvim_tree_highlight_opened_files = 1
-
--- -- This is the default. See :help filename-modifiers for more options
--- vim.g.nvim_tree_root_folder_modifier = ':~'
-
--- -- 0 by default, append a trailing slash to folder names
--- vim.g.nvim_tree_add_trailing = 1
-
--- -- 0 by default, compact folders that only contain a single folder into one node in the file tree
--- vim.g.nvim_tree_group_empty = 1
-
--- -- one space by default, used for rendering the space between the icon and the filename.
--- -- Use with caution, it could break rendering if you set an empty string depending on your font.
--- vim.g.nvim_tree_icon_padding = ' '
-
--- -- defaults to ' ➛ '. used as a separator between symlinks' source and target.
--- vim.g.nvim_tree_symlink_arrow = ' >> '
-
--- -- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
--- vim.g.nvim_tree_respect_buf_cwd = 1
-
--- -- 0 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
--- vim.g.nvim_tree_create_in_closed_folder = 1
-
--- -- List of filenames that gets highlighted with NvimTreeSpecialFile
--- vim.g.nvim_tree_special_files = { Makefile= 1, MAKEFILE= 1 }
-
--- -- If 0, do not show the icons for one of 'git' 'folder' and 'files'
--- -- 1 by default, notice that if 'files' is 1, it will only display
--- -- if nvim-web-devicons is installed and on your runtimepath.
--- -- if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
--- -- but this will not work when you set renderer.indent_markers.enable (because of UI conflict)
--- vim.g.nvim_tree_show_icons = {
---      git = 0,
---      folders = 1,
---      files = 1,
---      folder_arrows = 0,
---      }
-
-
-
--- -- following options are the default
--- -- each of these are documented in `:help nvim-tree.OPTION_NAME`
--- vim.g.nvim_tree_icons = {
---   default = "",
---   symlink = "",
---   git = {
---     unstaged = "",
---     staged = "S",
---     unmerged = "",
---     renamed = "➜",
---     deleted = "",
---     untracked = "U",
---     ignored = "◌",
---   },
---   folder = {
---     default = "",
---     open = "",
---     empty = "",
---     empty_open = "",
---     symlink = "",
---   },
--- }
-
-
-
 local function map(mode, lhs, rhs, opts)
     local options = {noremap = true, silent = true}
     if opts then options = vim.tbl_extend('force', options, opts) end
@@ -75,26 +5,6 @@ local function map(mode, lhs, rhs, opts)
 end
 
 map('n', '<leader>e', '<cmd>NvimTreeToggle<CR>')
-
--- -------------------- map example --------------------
--- -- It adds motions like 25j and 30k to the jump list, so you can cycle
--- -- through them with control-o and control-i.
--- -- source: https://www.vi-improved.org/vim-tips/
--- map("n", "j", [[v:count ? (v:count > 5 ? "m'" . v:count : '') . 'j' : 'gj']], { expr = true })
--- map("n", "k", [[v:count ? (v:count > 5 ? "m'" . v:count : '') . 'k' : 'gk']], { expr = true })
--- -- <Tab> to navigate the completion menu
--- map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
--- map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
--- map("i", "<s-cr>", "<c-o>o")         -- adds new line below (insert)
--- map("i", "<c-cr>", "<c-o>O")         -- adds new line above (insert)
-
-
-
--- nnoremap <C-n> :NvimTreeToggle<CR>
--- nnoremap <leader>r :NvimTreeRefresh<CR>
--- nnoremap <leader>n :NvimTreeFindFile<CR>
-
-
 
 
 
@@ -104,13 +14,6 @@ if not status_ok then
   return
 end
 
--- ------------------ callback is deprecated ---------------------
--- local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
--- if not config_status_ok then
---   return
--- end
-
--- local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
   auto_reload_on_write = true,
